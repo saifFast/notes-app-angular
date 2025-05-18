7d46e6cd085e479106c3b6e15163948b847d9b4a
saifFast <saifullahhakro@gmail.com>
2025-05-18 16:32:40 +0500

    update in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'notes-app-saif';
+  newNote: string = '';
+  notes: Note[] = [];
+
+  colors: string[] = [
+    '#fff9c4', // Light Yellow
+    '#c8e6c9', // Light Green
+    '#ffe0b2', // Light Orange
+    '#bbdefb', // Light Blue
+    '#f8bbd0', // Light Pink
+    '#d1c4e9', // Light Purple
+    '#b2ebf2', // Light Cyan
+  ];
+
+  
+  addNote(): void {
+    if (this.newNote.trim()) {
+      const noteColor = this.colors[Math.floor(Math.random() * this.colors.length)];
+      this.notes.unshift({ text: this.newNote.trim(),  color: noteColor });
+      this.newNote = '';
+    }
+  }
+  deleteNote(index: number): void {
+    this.notes.splice(index, 1);
+  }
+
+}
+
+interface Note {
+  text: string;
+  color: string;
 }
