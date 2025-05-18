84a0f3cb342c2c76ccefa8dc1f181fcd18a2e1aa
saifFast <saifullahhakro@gmail.com>
2025-05-18 16:45:37 +0500

    removed unused variable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
